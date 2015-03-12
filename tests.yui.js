de27c19ce412c0f4edab8bdd3b195176ed3fa3c5
Franck Marcia <franck.marcia@gmail.com>
2015-03-12 10:14:52 +0100

    refactor tests manager

--- tests.yui.js
+++ tests.yui.js
@@ -1,42 +1,53 @@
 #! /usr/bin/env node
 
-// var stuff
-var util = require( 'util' ),
-    fs = require( 'fs' ),
-    uglifycss = require( './uglifycss-lib' ),
-    path = '../yuicompressor/tests',
-    files = fs.readdirSync(path).sort(),
-    file, i, ugly, failed = 0, total = 0;
+(function () {
 
-// trim results (some ref minified files have new lines at the end)
-function trim( str ) {
-    return str.toString().replace( /(^\s*|\s*$)/g, '' );
-}
+    "use strict";
 
-// remove previous failures
-for ( i in files ) {
-    if ( /\.FAILED$/.test( files[ i ] ) ) {
-        fs.unlink( path + '/' + files[ i ] );
+    // path to yui tests
+    var PATH = "../yuicompressor/tests";
+
+    // dependancies
+    var util = require("util");
+    var fs = require("fs");
+    var uglifycss = require("./uglifycss-lib");
+
+    // trim results (some ref minified files got trailing new lines)
+    function trim(str) {
+        return str.toString().replace(/(^\s*|\s*$)/g, "");
     }
-}
 
-// check files
-for ( i in files ) {
-    file = path + '/' + files[ i ];
-    if ( /\.css$/.test( file ) ) {
-        ugly = uglifycss.processFiles( [ file ] );
-        if ( trim( ugly ) != trim( fs.readFileSync( file + '.min' ) ) ) {
-            util.puts( file + ': FAILED' );
-            fs.writeFile( file + '.FAILED', ugly );
-            failed += 1;
+    // get sorted files list and init counters
+    var files = fs.readdirSync(PATH).sort();
+    var failed = 0;
+    var total = 0;
+
+    // remove previous failures
+    files.forEach(function (file) {
+        if (/\.FAILED$/.test(file)) {
+            fs.unlink(PATH + "/" + file);
         }
-        total += 1;
-    }
-}
+    });
 
-// report total
-if ( failed ) {
-    console.log( total + ' tests, ' + failed + ' failed' );
-} else {
-    console.log( total + ' tests, no failure!' );
-}
+    // check files
+    files.forEach(function (file) {
+        file = PATH + "/" + file;
+        if (/\.css$/.test(file)) {
+            var ugly = uglifycss.processFiles([ file ]);
+            if (trim(ugly) !== trim(fs.readFileSync(file + ".min"))) {
+                util.puts(file + ": FAILED");
+                fs.writeFile(file + ".FAILED", ugly);
+                failed += 1;
+            }
+            total += 1;
+        }
+    });
+
+    // report total
+    if (failed) {
+        console.log(total + " tests, " + failed + " failed");
+    } else {
+        console.log(total + " tests, no failure!");
+    }
+
+}());
